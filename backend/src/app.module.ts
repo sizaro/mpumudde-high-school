39eff3ab71d080fd1006a802f6d6232177e9fc74
RONALD BAMUTESIZA <spartansiza@gmail.com>
2026-07-09 16:09:38 +0300

    database initiated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { StudentsModule } from './students/students.module';
+import { ParentsModule } from './parents/parents.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { ClassesModule } from './classes/classes.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { FinanceModule } from './finance/finance.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { TeachingAssignmentsModule } from './teaching-assignments/teaching-assignments.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, StudentsModule, ParentsModule, TeachersModule, ClassesModule, SubjectsModule, AttendanceModule, FinanceModule, UsersModule, RolesModule, TeachingAssignmentsModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
